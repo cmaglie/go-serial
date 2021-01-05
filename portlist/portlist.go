920d19e201372a9e6ef6e2d2ce93c18ce6d8968a
Cristian Maglie <c.maglie@bug.st>
2021-01-05 18:49:10 +0100

    linux: Enumerate USB interface name

--- portlist/portlist.go
+++ portlist/portlist.go
@@ -33,11 +33,15 @@ func main() {
 	for _, port := range ports {
 		fmt.Printf("Port: %s\n", port.Name)
 		if port.Product != "" {
-			fmt.Printf("   Product Name: %s\n", port.Product)
+			fmt.Printf("   Product Name  : %s\n", port.Product)
 		}
 		if port.IsUSB {
-			fmt.Printf("   USB ID      : %s:%s\n", port.VID, port.PID)
-			fmt.Printf("   USB serial  : %s\n", port.SerialNumber)
+			fmt.Printf("   USB ID        : %s:%s\n", port.VID, port.PID)
+			fmt.Printf("   USB serial    : %s\n", port.SerialNumber)
+			if port.USBInterface != "" {
+				fmt.Printf("   USB Interface : %s\n", port.USBInterface)
+
+			}
 		}
 	}
 }
